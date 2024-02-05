34a3106f1584115445641078a3c3d8241bc707df
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2024-02-05 15:51:18 -0600

    fix?

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ setup(
     version='0.0.0',
     packages=find_packages(),
     install_requires=[
-        'dictionaryutils',
-    ],
-    dependency_links=[
-       "git+https://github.com/uc-cdis/dictionaryutils.git@master#egg=dictionaryutils",
+        'dictionaryutils<4',
     ],
     package_data={
         "gdcdictionary": [
