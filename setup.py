b3ad3b8a0aea3b60cedd65c96b10f23238cb3df1
Alexander VanTol <Avantol13@users.noreply.github.com>
2024-02-06 16:28:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ setup(
     version='0.0.0',
     packages=find_packages(),
     install_requires=[
-        'dictionaryutils',
     ],
     dependency_links=[
-       "git+https://github.com/uc-cdis/dictionaryutils.git@3.2.0#egg=dictionaryutils",
     ],
     package_data={
         "gdcdictionary": [
